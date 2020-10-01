1713655a80e58590050ed3fd8ace18a77143c3b1
Tudos <tudosm@amazon.com>
2020-10-01 11:53:48 +0300

    added the ejs as the template engine and configured the app to use views

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const express = require('express');
 //assign the app to use the API
 const app = express();
 
+//set the app to use views folder for more organised app
+app.set('views', 'views')
+
+//setting the app to use the ejs as the view engine
+//needs to be installed via npm
+app.set('view engine', 'ejs')
+
 //get / or home path
 app.get('/', function(req, res){
-    res.send('</h1>welcome</h1>');
+    res.render('home-guest')
 });
 
 //get the application to listen to port 3000
